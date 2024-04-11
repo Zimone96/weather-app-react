f299b99ac07692c3508977b9626f552e11cf6b6b
Zimone96 <zimonesalome@gmail.com>
2024-04-11 10:18:52 +0200

    link

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { createRoot } from "react-dom/client";
 import Form from "./Form";
 import "./index.css";
 
-import ReactAnimatedWeather from "react-animated-weather";
+
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
@@ -16,6 +16,7 @@ root.render(
         <Form />
 
         <h2 className="mt-1">Tue 09 Apr '24</h2>
+        <a href="https://github.com/Zimone96/weather-app-react">GitHub</a>
       </div>
     </div>
   </StrictMode>,
